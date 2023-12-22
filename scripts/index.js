854e9cef1f304b08ec4a2b315915c7e5562987f8
AkakiGoginava <akakigoginava2808@gmail.com>
2023-12-22 16:07:20 +0400

    Add funcionality to save category button states to filter array

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+let filter = [] // Array to store chosen categories to filter
 let categoriesHTML = ''; // Variable to contain category HTML string for insertion 
 
 // Fetch category data
@@ -19,28 +20,37 @@ fetch('https://api.blog.redberryinternship.ge/api/categories')
     })
     // Make category buttons interactive
     .then(() => {
+        // Iterate over each category button to add functionality
         document.querySelectorAll('.js-category-button')
             .forEach((categoryButton) => {
-                // Check the local storage for button states         
-                const state = localStorage.getItem(categoryButton.id);
-                if (state === 'true') {
-                    categoryButton.classList.add('category-button-toggled');
-                } else if (state === 'false') {
-                    categoryButton.classList.remove('category-button-toggled');
-                }
-                // Add event listeners to category buttons
                 categoryButton.addEventListener('click', (event) => {
+                    // Add event listeners to category buttons
                     if (event.target.classList.contains('category-button-toggled')) {
                         event.target.classList.remove('category-button-toggled');
-                        // Save the state of the button to local storage
+                        // Remove from filter
+                        const index = filter.indexOf(`${categoryButton.id}`);
+                        filter.splice(index, 1);
+                        // Save the state of the category button to local storage
                         localStorage.setItem(event.target.id, 'false');
                     } else {
                         event.target.classList.add('category-button-toggled');
-                        // Save the state of the button to local storage
+                        // Add to filter
+                        filter.push(categoryButton.id);
+                        // Save the state of the category button to local storage
                         localStorage.setItem(event.target.id, 'true');
                     }})
+                    // Check the local storage for category button states
+                    const state = localStorage.getItem(categoryButton.id);
+                    if (state === 'true') {
+                        // Update the category button state
+                        categoryButton.classList.add('category-button-toggled');
+                        // Update the filter
+                        filter.push(categoryButton.id);
+                    }
             })
     })
     .catch(error => console.error('Error:', error))
+
+    console.log(filter)
         
 
