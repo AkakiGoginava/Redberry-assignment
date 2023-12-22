1ffa0df45529b306191205a292d8f481892cfe22
AkakiGoginava <akakigoginava2808@gmail.com>
2023-12-22 14:54:54 +0400

    Make category buttons interactive

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-let categoriesHTML = '';
+let categoriesHTML = ''; // Variable to contain category HTML string for insertion 
+
 // Fetch category data
 fetch('https://api.blog.redberryinternship.ge/api/categories')
     .then(response => response.json())
@@ -7,7 +8,7 @@ fetch('https://api.blog.redberryinternship.ge/api/categories')
     .then(categories => {
         categories.data.forEach((category) => {
             categoriesHTML += `
-            <button class='category-button' style='
+            <button class='category-button js-category-button' style='
             color:${category.text_color};
             background-color:${category.background_color};'>
             ${category.title}
@@ -15,6 +16,18 @@ fetch('https://api.blog.redberryinternship.ge/api/categories')
         })
         // Insert HTML into category section to display categories
         document.querySelector('.js-categories').innerHTML = categoriesHTML;
+    })
+    //Make category buttons interactive
+    .then(() => {
+        document.querySelectorAll('.js-category-button')
+            .forEach((button) => {
+                button.addEventListener('click', (event) => {
+                    if (event.target.classList.contains('category-button-toggled')) {
+                        event.target.classList.remove('category-button-toggled')
+                    } else {
+                        event.target.classList.add('category-button-toggled')
+                    }})
+            })
     })
     .catch(error => console.error('Error:', error))
         
