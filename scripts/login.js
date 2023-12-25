3cd72e2b8ee3d532f1db9df80909e6c9de705483
AkakiGoginava <akakigoginava2808@gmail.com>
2023-12-25 18:32:26 +0400

    Fix login 'POST' returning status 200

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,11 @@
+const emailPattern = /^[a-zA-Z0-9._-]+@redberry.ge/;
+const loginInputButton = document.querySelector('.login-input-button');
+
+// Storage for email input
+let emailData = {
+    email : null
+};
+
 function closeOverlay() {
     document.querySelector('.login-overlay-background')
         .classList.remove('login-overlay-background-on');
@@ -5,39 +13,43 @@ function closeOverlay() {
         .classList.remove('login-overlay-on');
 }
 
-// Storage for email input
-let emailData = {
-    email : null
-};
-
 function retrieveInput() {
     emailData.email = document.querySelector('.login-input').value
 }
 
-let emailPattern = /^[a-zA-Z0-9._-]+@redberry.ge/;
+function checkResponse(response) {
+    if(response.ok) {
+        document.querySelector('.login-input-block')
+            .classList.add('login-input-block-off');
+        document.querySelector('.login-success-block')
+            .classList.add('login-success-block-on');
+        document.querySelector('.login-button')
+            .textContent = 'დაამატე ბლოგი';
+        loginInputButton.textContent = 'კარგი';
+        loginInputButton.removeEventListener('click', retrieveInput)
+        loginInputButton.removeEventListener('click', verifyEmail)
+        loginInputButton.addEventListener('click', closeOverlay)
+    } else {
+        document.querySelector('.login-input')
+            .classList.add('login-input-error');
+        document.querySelector('.login-error-message')
+            .classList.add('login-error-message-on');
+    }
+}
 
 function verifyEmail() {
+    console.log(JSON.stringify(emailData));
     if (emailPattern.test(emailData.email)) {
         fetch('https://api.blog.redberryinternship.ge/api/login', {
-            method: 'POST', 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }, 
             body: JSON.stringify(emailData)})
             .then(response => {
-                // Check response
-                if(response.ok) {
-                    document.querySelector('.login-input-block')
-                        .classList.add('login-input-block-off');
-                    document.querySelector('.login-success-block')
-                        .classList.add('login-success-block-on');
-                    document.querySelector('.login-button')
-                        .textContent = 'დაამატე ბლოგი';
-                    loginInputButton.textContent = 'კარგი';
-                    loginInputButton.removeEventListener('click', retrieveInput)
-                    loginInputButton.removeEventListener('click', verifyEmail)
-                    loginInputButton.addEventListener('click', closeOverlay)
-                } else {
-                    document.querySelector('.login-input')
-                        .classList.add('login-input-error');
-                }})
+                checkResponse(response);
+               })
     } else {
         document.querySelector('.login-input')
             .classList.add('login-input-error');
@@ -57,11 +69,6 @@ document.querySelector('.login-button').addEventListener('click', () => {
 // Add event listener to login exit button
 document.querySelector('.login-exit-button').addEventListener('click', closeOverlay);
 
-const loginInputButton = document.querySelector('.login-input-button');
-
-
 // Add event listener to input button to retrieve input
 loginInputButton.addEventListener('click', retrieveInput);
 loginInputButton.addEventListener('click', verifyEmail);
-
-
